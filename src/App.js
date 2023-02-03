d95c1ceafecef855e59bd986641fb2b309cff342
Your Name <you@example.com>
2023-02-03 16:11:19 +0530

    Demo Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
+import Calender from './components/calender'
+function App() {
+  return (
+    <div className="App">
+ <Calender/>
+    </div>
+  );
+}
+
+export default App;
